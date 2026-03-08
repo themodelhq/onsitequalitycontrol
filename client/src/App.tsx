2733bdf0825f1861668ac8d2413943ef4b031c51
themodelhq <themodelhq@gmail.com>
2026-03-08 20:14:17 +0100

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,40 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch } from "wouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Home from "./pages/Home";
+import Upload from "./pages/Upload";
+import Results from "./pages/Results";
+import ProductDetail from "./pages/ProductDetail";
+import Reports from "./pages/Reports";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/upload" component={Upload} />
+      <Route path="/results" component={Results} />
+      <Route path="/product/:id" component={ProductDetail} />
+      <Route path="/reports" component={Reports} />
+      <Route path="/404" component={NotFound} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="light">
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
